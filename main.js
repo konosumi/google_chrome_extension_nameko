68efe66d32d2f56a5948029375544c0f8b55ab0d
konosumi <36559308+konosumi@users.noreply.github.com>
2018-05-27 15:35:36 +0900

    不具合を修正

--- main.js
+++ main.js
@@ -2,17 +2,19 @@
 $(document).ready(function() {
     var namekoImage = "https://raw.githubusercontent.com/konosumi/blog_0194_learning_google_chrome_extension_with_nameko/master/icons/nameko.jpg";
 
-    var topHpLogo = $("img#hplogo");
-    topHpLogo.attr("src", namekoImage);
-    topHpLogo.attr("srcset", namekoImage);
-    topHpLogo.attr("width", "80");
-    topHpLogo.attr("height", "80");
-
+    // 検索ページのロゴ
     var searchLogo = $("a#logo").children('img');
     searchLogo.attr("src", namekoImage);
     searchLogo.attr("srcset", namekoImage);
     searchLogo.attr("width", "60");
     searchLogo.attr("height", "60");
+
+    // トップページのロゴ
+    var topHpLogo = $("img#hplogo");
+    topHpLogo.attr("src", namekoImage);
+    topHpLogo.attr("srcset", namekoImage);
+    topHpLogo.attr("width", "80");
+    topHpLogo.attr("height", "80");
 });
 
 

9d2758d5270fd40076049761e8f4eba28c27fff8
konosumi <36559308+konosumi@users.noreply.github.com>
2018-05-27 15:27:34 +0900

    fix

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 
 $(document).ready(function() {
-    $("img#hplogo").attr("src", "http://img.f.hatena.ne.jp/images/fotolife/k/konosumi/20171111/20171111021102.png");
-    $("img#hplogo").attr("srcset", "http://img.f.hatena.ne.jp/images/fotolife/k/konosumi/20171111/20171111021102.png");
+    var namekoImage = "https://raw.githubusercontent.com/konosumi/blog_0194_learning_google_chrome_extension_with_nameko/master/icons/nameko.jpg";
+
+    var topHpLogo = $("img#hplogo");
+    topHpLogo.attr("src", namekoImage);
+    topHpLogo.attr("srcset", namekoImage);
+    topHpLogo.attr("width", "80");
+    topHpLogo.attr("height", "80");
+
+    var searchLogo = $("a#logo").children('img');
+    searchLogo.attr("src", namekoImage);
+    searchLogo.attr("srcset", namekoImage);
+    searchLogo.attr("width", "60");
+    searchLogo.attr("height", "60");
 });
 
 

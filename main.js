c2b5db7491619ffcc0ec87d55937e903d42822bb
konosumi <36559308+konosumi@users.noreply.github.com>
2018-05-27 15:05:03 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+    $("img#hplogo").attr("src", "http://img.f.hatena.ne.jp/images/fotolife/k/konosumi/20171111/20171111021102.png");
+    $("img#hplogo").attr("srcset", "http://img.f.hatena.ne.jp/images/fotolife/k/konosumi/20171111/20171111021102.png");
+});
+
+
